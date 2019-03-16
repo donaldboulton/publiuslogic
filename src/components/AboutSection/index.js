2754f88e38dc388f30165a93411719e971942006
Donald Boulton <donaldboulton@gmail.com>
2019-03-16 13:59:22 -0500

    rebuikd no icon files just svgs

--- src/components/AboutSection/index.js
+++ src/components/AboutSection/index.js
@@ -5,7 +5,12 @@ import logo from '../../img/apple-touch-icon-round.png'
 import react from '../../img/react.svg'
 import lambda from '../../img/lambda.png'
 import netlify from '../../img/logomark.svg'
-import { FaBalanceScale, FaTools, FaTwitter, FaExternalLinkAlt, FaGithub, FaFacebookF } from 'react-icons/fa'
+import BalanceScale from '../../img/balance-scale.svg'
+import Tools from '../../img/tools.svg'
+import Twitter from '../../img/twitter.svg'
+import Github from '../../img/github.svg'
+import ExternalLinkAlt from '../../img/external-link-alt.svg'
+import FacebookF from '../../img/facebook-f.svg'
 
 const AboutSection = () => {
   return (
@@ -42,9 +47,9 @@ const AboutSection = () => {
             <div className='content'>
               Donald Boulton uses GitHub via a continuous deployment
               <br />
-              <div><a href='https://github.com/donaldboulton/' rel='me'><span className='icon is-small has-text-light'><FaGithub /></span>&nbsp;My GitHub Repos</a></div>
-              <div><a href='https://twitter.com/donboulton' rel='me'><span className='icon is-small has-text-info'><FaTwitter /></span>&nbsp;Twitter @donboulton.com</a></div>
-              <div><a href='https://facebook.com/don.boulton' rel='me'><span className='icon is-small has-text-link'><FaFacebookF /></span>&nbsp;Facebook don.boulton</a></div>
+              <div><a href='https://github.com/donaldboulton/' rel='me'><span className='icon is-small has-text-light'><Github /></span>&nbsp;My GitHub Repos</a></div>
+              <div><a href='https://twitter.com/donboulton' rel='me'><span className='icon is-small has-text-info'><Twitter /></span>&nbsp;Twitter @donboulton.com</a></div>
+              <div><a href='https://facebook.com/don.boulton' rel='me'><span className='icon is-small has-text-link'><FacebookF /></span>&nbsp;Facebook don.boulton</a></div>
               <br />
               <span>
                 <a href='https://donboulton.com'><span className='icon is-small has-text-light'><FaExternalLinkAlt /></span>&nbsp;Designer donboulton.com</a>
@@ -84,11 +89,11 @@ const AboutSection = () => {
             <div className='content'>
                         Gatsby is Easy to learn even for novice developers
               <br />
-              <div><a href='https://www.gatsbyjs.org/'><span className='icon is-small has-text-light'><FaExternalLinkAlt /></span>&nbsp;Framework Gatsby -v2.1</a></div>
-              <div><a href='https://bulma.io/'><span className='icon is-small has-text-light'><FaExternalLinkAlt /></span>&nbsp;CSS from @bulma.io</a></div>
-              <div><a href='https://www.gatsbyjs.org/packages/gatsby-plugin-styled-components/'><span className='icon is-small has-text-light'><FaExternalLinkAlt /></span>&nbsp;Styled Components Icons</a></div>
+              <div><a href='https://www.gatsbyjs.org/'><span className='icon is-small has-text-light'><ExternalLinkAlt /></span>&nbsp;Framework Gatsby -v2.1</a></div>
+              <div><a href='https://bulma.io/'><span className='icon is-small has-text-light'><ExternalLinkAlt /></span>&nbsp;CSS from @bulma.io</a></div>
+              <div><a href='https://www.gatsbyjs.org/packages/gatsby-plugin-styled-components/'><span className='icon is-small has-text-light'><ExternalLinkAlt /></span>&nbsp;Styled Components Icons</a></div>
               <br />
-              <a href='https://publiuslogic.com'><span><span className='icon is-small has-text-light'><FaBalanceScale /></span><span>&nbsp;Terms and Conditions.</span></span></a>
+              <a href='https://publiuslogic.com'><span><span className='icon is-small has-text-light'><BalanceScale /></span><span>&nbsp;Terms and Conditions.</span></span></a>
             </div>
           </div>
         </div>
@@ -123,11 +128,11 @@ const AboutSection = () => {
             <div className='content'>
               Netlify forms posted to slack with Lambda Functions
               <br />
-              <div><a title='Netlify' href='https://netlify.com' target='_blank' rel='noopener noreferrer'><span className='icon is-small has-text-light'><FaExternalLinkAlt /></span>&nbsp;Netlify Home Page</a></div>
-              <div><a href='https://www.netlify.com/features/'><span className='icon is-small has-text-light'><FaExternalLinkAlt /></span>&nbsp;SLL Security</a></div>
-              <div><a href='https://app.netlify.com/signup?_ga=2.257002891.1243890719.1552279568-538917114.1550763563'><span className='icon is-small has-text-light'><FaExternalLinkAlt /></span>&nbsp;Netlify Free Tier</a></div>
+              <div><a title='Netlify' href='https://netlify.com' target='_blank' rel='noopener noreferrer'><span className='icon is-small has-text-light'><ExternalLinkAlt /></span>&nbsp;Netlify Home Page</a></div>
+              <div><a href='https://www.netlify.com/features/'><span className='icon is-small has-text-light'><ExternalLinkAlt /></span>&nbsp;SLL Security</a></div>
+              <div><a href='https://app.netlify.com/signup?_ga=2.257002891.1243890719.1552279568-538917114.1550763563'><span className='icon is-small has-text-light'><ExternalLinkAlt /></span>&nbsp;Netlify Free Tier</a></div>
               <br />
-              <a href='https://publiuslogic.com/admin/'><span><span className='icon is-small has-text-light'><FaTools /></span><span>&nbsp;Admin - Netlify CMS</span></span></a>
+              <a href='https://publiuslogic.com/admin/'><span><span className='icon is-small has-text-light'><Tools /></span><span>&nbsp;Admin - Netlify CMS</span></span></a>
             </div>
           </div>
         </div>
