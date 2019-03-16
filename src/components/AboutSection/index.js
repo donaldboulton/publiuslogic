bb3772fdca4def67e6a865b954b474bdb1b5c382
Donald Boulton <donaldboulton@gmail.com>
2019-03-16 15:14:19 -0500

    icon issues

--- src/components/AboutSection/index.js
+++ src/components/AboutSection/index.js
@@ -5,10 +5,8 @@ import logo from '../../img/logo.png'
 import react from '../../img/react.svg'
 import lambda from '../../img/lambda.png'
 import netlify from '../../img/logomark.svg'
-import BalanceScale from '../../img/balance-scale.svg'
-import Tools from '../../img/tools.svg'
-import Twitter from '../../img/twitter.svg'
-import FacebookF from '../../img/facebook-f.svg'
+import balancescale from '../../img/balance-scale.svg'
+import tools from '../../img/tools.svg'
 
 const AboutSection = () => {
   return (
@@ -45,12 +43,12 @@ const AboutSection = () => {
             <div className='content'>
               Donald Boulton uses GitHub via a continuous deployment
               <br />
-              <div><a href='https://github.com/donaldboulton/' rel='me'><span className='icon is-small has-text-light'>{github}</span>&nbsp;My GitHub Repos</a></div>
-              <div><a href='https://twitter.com/donboulton' rel='me'><span className='icon is-small has-text-info'>{Twitter}</span>&nbsp;Twitter @donboulton.com</a></div>
-              <div><a href='https://facebook.com/don.boulton' rel='me'><span className='icon is-small has-text-link'>{FacebookF}</span>&nbsp;Facebook don.boulton</a></div>
+              <div><a href='https://github.com/donaldboulton/' rel='me'>&nbsp;DWB GitHub Repos</a></div>
+              <div><a href='https://twitter.com/donboulton' rel='me'>&nbsp;Twitter @donboulton.com</a></div>
+              <div><a href='https://facebook.com/don.boulton' rel='me'>&nbsp;Facebook don.boulton</a></div>
               <br />
               <span>
-                <a href='https://donboulton.com'>Designer donboulton.com</a>
+                <a href='https://donboulton.com'>DWB - donboulton.com</a>
               </span>
             </div>
           </div>
@@ -89,9 +87,9 @@ const AboutSection = () => {
               <br />
               <div><a href='https://www.gatsbyjs.org/'>Framework Gatsby -v2.1</a></div>
               <div><a href='https://bulma.io/'>CSS from @bulma.io</a></div>
-              <div><a href='https://www.gatsbyjs.org/packages/gatsby-plugin-styled-components/'>Styled Components Icons</a></div>
+              <div><a href='https://fontawesome.com/icons'>FontAwsome SVG's</a></div>
               <br />
-              <a href='https://publiuslogic.com'><span><span className='icon is-small has-text-light'>{BalanceScale}</span><span>&nbsp;Terms and Conditions.</span></span></a>
+              <a href='https://publiuslogic.com'><span><span className='icon is-small has-text-light'>{balancescale}</span><span>&nbsp;Terms and Conditions.</span></span></a>
             </div>
           </div>
         </div>
@@ -130,7 +128,7 @@ const AboutSection = () => {
               <div><a href='https://www.netlify.com/features/'>SLL Security</a></div>
               <div><a href='https://app.netlify.com/signup?_ga=2.257002891.1243890719.1552279568-538917114.1550763563'>Netlify Free Tier</a></div>
               <br />
-              <a href='https://publiuslogic.com/admin/'><span><span className='icon is-small has-text-light'>{Tools}</span><span>&nbsp;Admin - Netlify CMS</span></span></a>
+              <a href='https://publiuslogic.com/admin/'><span><span className='icon is-small has-text-light'>{tools}</span><span>&nbsp;Admin - Netlify CMS</span></span></a>
             </div>
           </div>
         </div>
