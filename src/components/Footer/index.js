a44a3efb8592be3e9d27b1407dbe3e88ad347878
Donald Boulton <donaldboulton@gmail.com>
2019-03-20 18:53:27 -0500

    nav mega back

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,10 +5,11 @@ const Footer = () => {
   return (
     <footer className='footer'>
       <div className='container'>
-        <div className='content has-text-centered social-links'>
+        <div className='content has-text-centered'>
           <p>
             {config.copyright}
           </p>
+          <br />
           <p>Powered by <a href='https://www.gatsbyjs.org'><strong>Gatsby</strong></a> & <a href='https://www.netlifycms.org'><strong>Netlify CMS&nbsp;| </strong></a><a href='https://www.netlify.com/docs/identity/'><strong>Netlify Identity&nbsp; |</strong></a><a href='https://github.com/donaldboulton/publiuslogic'><strong>&nbsp;GitHub Repository</strong></a>
           </p>
         </div>
