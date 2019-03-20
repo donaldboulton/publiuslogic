12aa0f61b5f32dd3f366ac48c43392f66d79e233
Donald Boulton <donaldboulton@gmail.com>
2019-03-20 15:41:15 -0500

    fix to norm menu

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {Link, graphql, StaticQuery} from 'gatsby'
 import SearchBox from '../SearchBox'
 import logo from '../../img/site-logo250x80.png'
-
+import rss from '../../img/rss.svg'
 import NetlifyIdentityWidget from '../IdentityWidget'
 
 const NavBar = ({toggleNavbar, isActive}) => (
@@ -56,28 +56,30 @@ const NavBar = ({toggleNavbar, isActive}) => (
                   Privacy & terms
                 </a>
               </div>
-              <div className='navbar-item has-dropdown is-hoverable'>
+            </div>
+            <div className='navbar-item has-dropdown is-hoverable'>
               <a className='navbar-link  is-active' href='/'>
                  Docs
               </a>
               <div className='navbar-dropdown'>
-                <a className='navbar-item ' href='/blog'>
-                  Blog
+                <a className='navbar-item ' href='/'>
+                 Home
                 </a>
-                <a className='navbar-item ' href='/blog/just-in-small-batch-of-jamaican-blue-mountain-in-store-next-week/'>
-                  Blue Mountain
+                <a className='navbar-item ' href='/about'>
+                 About
                 </a>
-                <a className='navbar-item is-active' href='/blog/making-sense-of-the-scaas-new-flavor-wheel/'>
-                  Flavor Wheel
+                <a className='navbar-item is-active' href='/pricing'>
+                  Pricing
                 </a>
                 <a className='navbar-item ' href='/contact'>
-                  Chemex Brewing
+                  Contact
                 </a>
                 <a className='navbar-item ' href='/privacy'>
-                  lorem-ipsum-dolor-situm
+                  Privacy & terms
                 </a>
               </div>
             </div>
+
             <div className='navbar-item site-title' id='login'>
               <NetlifyIdentityWidget />
             </div>
