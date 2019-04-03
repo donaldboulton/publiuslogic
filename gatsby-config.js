3d6596ae608cab28aa219f989ec5f2293e84cd83
Donald Boulton <donaldboulton@gmail.com>
2019-04-03 15:09:07 -0500

    ok

--- gatsby-config.js
+++ gatsby-config.js
@@ -85,6 +85,17 @@ module.exports = {
         path: `${__dirname}/src/pages/privacy`,
       },
     },
+    {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        // your google analytics tracking id
+        trackingId: `UA-24847941-1`,
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // enable ip anonymization
+        anonymize: true,
+      },
+    },
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     {
--- src/html.js
+++ src/html.js
@@ -23,10 +23,8 @@ export default class HTML extends Component {
           <script
             dangerouslySetInnerHTML={{
               __html: `
-                      <!-- Google Tag Manager (noscript) -->
-                      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WLCMLLP"
-                      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-                      <!-- End Google Tag Manager (noscript) -->
+                      var name = 'world';
+                      console.log('Hello ' + name);
                     `,
             }}
           />
