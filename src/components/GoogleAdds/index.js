927c8f47ef398a7c13f0e9fe07a9d665c720523e
Donald Boulton <donaldboulton@gmail.com>
2019-04-04 07:41:08 -0500

    adds react component

--- src/components/GoogleAdds/index.js
+++ src/components/GoogleAdds/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import Helmet from 'react-helmet'
-import { OutboundLink } from 'gatsby-plugin-google-gtag'
+import AdSense from 'react-adsense'
 
 const Adds = () => {
   return (
@@ -8,17 +7,13 @@ const Adds = () => {
       <div className='column is-10 is-offset-1'>
         <div className='content'>
           <div role='note' className='add-block clear-both' itemScope itemType='https://schema.org/WPAdBlock'>
-            <Helmet>
-              <OutboundLink href="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" />
-              <meta itemProp='name' content='publiuslogic.com' />
-              <meta itemProp='description' content='Support PubliusLogic by using AdSense and other advertisements.' />
-              <ins className='adsbygoogle'
-                style='display:block'
-                data-ad-client='ca-pub-7655495105068461'
-                data-ad-slot='5601054893'
-                data-ad-format='auto' />
-                (adsbygoogle = window.adsbygoogle || []).push({});
-            </Helmet>              
+            <AdSense.Google
+              client='ca-pub-7655495105068461'
+              slot='5601054893'
+              style={{ display: 'block' }}
+              layout='in-article'
+              format='fluid'
+            />
           </div>
         </div>
       </div>
