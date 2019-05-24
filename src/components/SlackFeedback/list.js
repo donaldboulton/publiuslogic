674792f1e465954b3631cf18526947ec4be1b55b
Donald Boulton <donaldboulton@gmail.com>
2019-05-24 10:15:55 -0500

    move git to root

--- src/components/SlackFeedback/list.js
+++ src/components/SlackFeedback/list.js
@@ -1,38 +1,37 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
 class List extends Component {
   // Initialize the state
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
-      list: []
-    };
+      list: [],
+    }
   }
 
   // Fetch the list on first mount
-  componentDidMount() {
-    this.getList();
+  componentDidMount () {
+    this.getList()
   }
 
   // Retrieves the list of items from the Express app
   getList = () => {
-    fetch("/api/items")
+    fetch('/api/items')
       .then(res => res.json())
-      .then(list => this.setState({ list }));
+      .then(list => this.setState({ list }))
   };
 
-  render() {
-    const { list } = this.state;
+  render () {
+    const { list } = this.state
 
     return (
       <div>
-        <h1 style={{ fontSize: "2em" }}>List of Items from Express Server</h1>
-        {/* Check to see if any items are found*/}
+        <h1 style={{ fontSize: '2em' }}>List of Items from Express Server</h1>
         {list.length ? (
           <div>
             {/* Render the list of items */}
             {list.map(item => {
-              return <div>{item}</div>;
+              return <div>{item}</div>
             })}
           </div>
         ) : (
