1409ae39e70f6bb785b3091397d42976d315d2e7
Donald Boulton <donaldboulton@gmail.com>
2019-06-02 09:13:02 -0500

    clean build

--- gatsby-config.js
+++ gatsby-config.js
@@ -117,6 +117,16 @@ module.exports = {
             },
           },
           {
+            resolve: 'gatsby-remark-embed-video',
+            options: {
+              width: 800,
+              ratio: 1.77, // Optional: Defaults to 16/9 = 1.77
+              height: 400, // Optional: Overrides optional.ratio
+              related: false, // Optional: Will remove related videos from the end of an embedded YouTube video.
+              noIframeBorder: true, // Optional: Disable insertion of <style> border: 0
+            },
+          },
+          {
             resolve: `gatsby-remark-autolink-headers`,
             options: {
               offsetY: `100`,
@@ -148,16 +158,6 @@ module.exports = {
               maxWidth: 2048,
               showCaptions: true,
               withWebp: true,
-            },
-          },
-          {
-            resolve: 'gatsby-remark-embed-video',
-            options: {
-              width: 800,
-              ratio: 1.77, // Optional: Defaults to 16/9 = 1.77
-              height: 400, // Optional: Overrides optional.ratio
-              related: false, // Optional: Will remove related videos from the end of an embedded YouTube video.
-              noIframeBorder: true, // Optional: Disable insertion of <style> border: 0
             },
           },
           {
