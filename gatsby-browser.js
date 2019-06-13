c9a7ceac6ed87d19be61df0c52e4f130f007097f
Donald Boulton <donaldboulton@gmail.com>
2019-06-13 07:33:21 -0500

    push notify click fix

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -32,5 +32,26 @@ export const onServiceWorkerUpdateFound = () => {
 
   showNotification()
 
-  const ExternalLinkModali = 'https://publiuslogic.com/blog/modali-hooks-modal/'
+  const url = 'https://publiuslogic.com/blog/modali-hooks-modal/'
+
+  self.addEventListener('notificationclick', function (event) {
+    let url = 'https://publiuslogic.com/blog/modali-hooks-modal/'
+    event.notification.close() // Android needs explicit close.
+    event.waitUntil(
+      clients.matchAll({ type: 'window' }).then(windowClients => {
+        // Check if there is already a window/tab open with the target URL
+        for (var i = 0; i < windowClients.length; i++) {
+          var client = windowClients[i]
+          // If so, just focus it.
+          if (client.url === url && 'focus' in client) {
+            return client.focus()
+          }
+        }
+        // If not, then open the target URL in a new window/tab.
+        if (clients.openWindow) {
+          return clients.openWindow(url)
+        }
+      })
+    )
+  })
 }
