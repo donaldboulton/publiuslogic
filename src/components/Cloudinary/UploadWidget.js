028a12459768d35c5a155ca61e0db2f3ea5ba433
Donald Boulton <donaldboulton@gmail.com>
2019-06-17 10:53:05 -0500

    UploadWidget

--- src/components/Cloudinary/UploadWidget.js
+++ src/components/Cloudinary/UploadWidget.js
@@ -3,7 +3,34 @@ import Helmet from 'react-helmet'
 
 class UploadWidget extends Component {
   uploadWidget () {
-    cloudinary.openUploadWidget({ cloud_name: 'mansbooks', upload_preset: 'photos-preset', tags: ['cats'], sources: ['local', 'url', 'dropbox'], dropboxAppKey: 'fk4ayp4zwevjgl7' },
+    cloudinary.openUploadWidget({
+      cloud_name: 'mansbooks',
+      upload_preset: 'photos-preset',
+      tags: ['cats'],
+      sources: ['local', 'url', 'camera', 'image_search', 'facebook', 'dropbox', 'instagram'],
+      dropboxAppKey: 'fk4ayp4zwevjgl7',
+      googleApiKey: 'AIzaSyCEL0HqEXvP42ZYK-xd7CBqO50-ZzLKwFM',
+      showAdvancedOptions: true,
+      cropping: true,
+      multiple: false,
+      defaultSource: 'local',
+      styles: {
+        palette: {
+          window: '#1D1D1D',
+          sourceBg: '#000000',
+          windowBorder: '#434040',
+          tabIcon: '#FFFFFF',
+          inactiveTabIcon: '#8E9FBF',
+          menuIcons: '#D64000',
+          link: '#D64000',
+          action: '#9C3204',
+          inProgress: '#D64000',
+          complete: '#33ff00',
+          error: '#EA2727',
+          textDark: '#000000',
+          textLight: '#FFFFFF'        
+        },
+      },
       function (error, result) {
         console.log(result)
       })
