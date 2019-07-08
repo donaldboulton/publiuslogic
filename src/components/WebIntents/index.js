fd67bb3d00701d82284d5426ad3327170166ea80
Donald Boulton <donaldboulton@gmail.com>
2019-07-08 11:40:12 -0500

    twitter button

--- src/components/WebIntents/index.js
+++ src/components/WebIntents/index.js
@@ -32,7 +32,7 @@ const WebIntents = ({ slug, tweet_id }) => {
                   data-show-count='true'
                   title={title}
                   key={url}
-                  className='twitter-follow-button button is-small twitter-btn'
+                  className='twitter-follow-button twitter-btn'
                   data-related='donboulton'
                   data-show-screen-name='false'
                   href='https://twitter.com/donboulton?ref_src=twsrc%5Etfw'
