789728ef81850b38c2f0b71f772fb809405f1b08
Donald Boulton <donaldboulton@gmail.com>
2019-07-24 12:58:10 -0500

    image folder change

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,7 +67,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {        
-        path: `${__dirname}/static/images/`,
+        path: `${__dirname}/static/images`,
         name: `images`,
       },
     },
