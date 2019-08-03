aa8c56c5e460655e847b369548e5974b9f92dd50
Donald Boulton <donaldboulton@gmail.com>
2019-08-03 12:10:52 -0500

    about error

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -5,44 +5,9 @@ import { graphql } from 'gatsby'
 import { HTMLContent } from '../components/Content'
 import AboutPageTemplate from '../components/AboutPageTemplate'
 import Global from '../components/Global'
-import config from '../../data/config'
 
 const AboutPage = ({ data }) => {
   const { markdownRemark: post } = data
-
-  const schemaOrgWebPage = {
-    '@context': 'http://schema.org',
-    '@type': 'WebPage',
-    url: 'https://publiuslogic.com/about',
-    title: 'Home | PubliusLogic',
-    inLanguage: config.siteLanguage,
-    mainEntityOfPage: 'https://publiuslogic.com/about',
-    description: config.siteDescription,
-    name: config.siteTitle,
-    author: {
-      '@type': 'Person',
-      name: config.author,
-    },
-    copyrightHolder: {
-      '@type': 'Person',
-      name: config.author,
-    },
-    copyrightYear: '2019',
-    creator: {
-      '@type': 'Person',
-      name: config.author,
-    },
-    publisher: {
-      '@type': 'Person',
-      name: config.author,
-    },
-    datePublished: '2019-07-12T10:30:00+01:00',
-    dateModified: '2019-07-12T10:30:00+01:00',
-    image: {
-      '@type': 'ImageObject',
-      url: config.siteLogo,
-    },
-  }
 
   return (
     <Global pageTitle={post.frontmatter.title}>
@@ -51,36 +16,11 @@ const AboutPage = ({ data }) => {
         <meta name='description' content={post.frontmatter.meta_description} />
         <meta name='keywords' content={post.frontmatter.tags} />
         <meta name='image' content={post.frontmatter.cover} />
-        <meta name='url' content={post.frontmatter.canonical} />
-        <meta property='og:type' content='article' />
-        <meta property='og:title' content={post.frontmatter.title} />
-        <meta property='og:description' content={post.frontmatter.meta_description} />
-        <meta property='og:image' content={post.frontmatter.cover} />
-        <meta property='og:image:alt' content={post.frontmatter.meta_title} />
-        <meta property='og:image:width' content='100%' />
-        <meta property='og:image:height' content='400px' />
-        <meta property='og:url' content={post.frontmatter.canonical} />
-        <meta name='rel' content={post.frontmatter.canonical} />
-        <meta name='key' content={post.frontmatter.canonical} />
-        <meta name='twitter:author' content='donboulton' />
-        <meta name='twitter:card' content='summary_large_image' />
-        <meta name='twitter:title' content={post.frontmatter.title} />
-        <meta name='twitter:image' content={post.frontmatter.cover} />
-        <meta name='twitter:description' content={post.frontmatter.meta_description} />
-        <meta name='twitter:widgets:autoload' content='off' />
-        <meta name='twitter:widgets:theme' content='dark' />
-        <meta name='twitter:widgets:link-color' content='#d64000' />
-        <meta name='twitter:widgets:border-color' content='#000000' />
-        <meta name='twitter:dnt' content='on' />
-        <link rel='canonical' href={post.frontmatter.canonical} />
-        <link rel='image_src' href={`${config.siteUrl}${config.logo}`} />
-        <link rel='me' href='https://twitter.com/donboulton' />
-        <script type='application/ld+json'>{JSON.stringify(schemaOrgWebPage)}</script>
+        <meta name='url' content={post.frontmatter.canonical} />      
       </Helmet>
       <AboutPageTemplate
         contentComponent={HTMLContent}
         title={post.frontmatter.title}
-        cover={post.frontmatter.cover}
         content={post.html}
       />      
     </Global>
@@ -97,7 +37,7 @@ export const aboutPageQuery = graphql`
   query AboutPage($id: String!) {
     markdownRemark(id: { eq: $id }) {
       html
-      post.frontmatter {
+      frontmatter {
         title   
         cover
         canonical   
