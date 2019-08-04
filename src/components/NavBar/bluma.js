2ac83368285d009816bf68746976e65aba577c7a
Donald Boulton <donaldboulton@gmail.com>
2019-08-04 07:25:51 -0500

    dropdown script for edge error

--- src/components/NavBar/bluma.js
+++ src/components/NavBar/bluma.js
@@ -1,17 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0)
-
-  if ($navbarBurgers.length > 0) {
-    $navbarBurgers.forEach(function ($el) {
-      $el.addEventListener('click', function () {
-        const target = $el.dataset.target
-        const $target = document.getElementById(target)
-        $el.classList.toggle('is-active')
-        $target.classList.toggle('is-active')
-      })
-    })
-  }
-})
 let dropdown = document.querySelector('.dropdown');
 dropdown.addEventListener('click', function (event) {
   event.stopPropagation()
