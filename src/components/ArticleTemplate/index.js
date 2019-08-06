f0a53008f01700cbe9924d144c78f8c23805fbe6
Donald Boulton <donaldboulton@gmail.com>
2019-08-06 06:39:52 -0500

    moment for time to read

--- src/components/ArticleTemplate/index.js
+++ src/components/ArticleTemplate/index.js
@@ -8,6 +8,7 @@ import { Calendar } from 'styled-icons/octicons/Calendar'
 import { Timer } from 'styled-icons/material/Timer'
 import { kebabCase } from 'lodash'
 import { Link } from 'gatsby'
+import moment from 'moment'
 import Prism from '../../utils/prism'
 
 require('prismjs')
@@ -23,7 +24,6 @@ require('prismjs/components/prism-diff')
 require('prismjs/components/prism-markdown')
 require('prismjs/components/prism-graphql')
 
-require('moment')
 
 const ArticleTemplate = ({
   content,
@@ -58,7 +58,7 @@ const ArticleTemplate = ({
             <Calendar size='.9em' />&nbsp;
             <Date>{date}&nbsp;</Date>&nbsp;
             <Timer size='.9em' />
-            <Time>{timeToRead} 3 min</Time>
+            <Time>{moment(timeToRead).local().format(`MMMM DD, YYYY`)}&nbsp;min</Time>
           </span>
         </div>
         <WebIntents />
