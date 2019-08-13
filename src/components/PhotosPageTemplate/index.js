69a737e52ded096b964338162f632f47bcdcc7b5
Donald Boulton <donaldboulton@gmail.com>
2019-08-12 19:50:26 -0500

    styled title on cat gone

--- src/components/PhotosPageTemplate/index.js
+++ src/components/PhotosPageTemplate/index.js
@@ -1,25 +1,16 @@
 import React from 'react'
-import styled from 'styled-components'
 import Content from '../Content'
 import Cloudinary from '../Cloudinary'
 import UploadWidget from '../Cloudinary/UploadWidget'
 import PropTypes from 'prop-types'
 import { media } from '../Hero/style'
 
-const SectionPageTitle = styled.h2`
-  font-size: 2em;
-  margin: 0.67em 0;
-  ${media.xs`
-    font-size:1.5em;
-  `}
-`
 const PhotosPageTemplate = ({ data, canonical, cover, title, meta_title, meta_description, content, contentComponent }) => {
   const PageContent = contentComponent || Content
 
   return (
     <div>
       <section className='section section--gradient'>
-        <SectionPageTitle>My Cats</SectionPageTitle>
         <div className='container'>
           <div className='is-10 is-offset-1'>
             <div>
