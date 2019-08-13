9f41d33ac41e5f012bf3f39bb9ad42fe98d0e93f
Donald Boulton <donaldboulton@gmail.com>
2019-08-13 13:12:35 -0500

    markdownremark

--- src/templates/article-page.js
+++ src/templates/article-page.js
@@ -47,7 +47,7 @@ const Styledh1 = styled.h1`
 `
 
 const ArticlePage = ({ data, timeToRead, html }) => {
-  const { markdownRemark: post, allRatingsJson: ratings = [], frontmatter } = data.markdownremark
+  const { markdownRemark: post, allRatingsJson: ratings = [], frontmatter } = markdownremark
 
   const ratingValue =
     ratings && ratings.edges
