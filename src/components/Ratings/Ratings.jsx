da6e341be1b479aaa0f8276dbaa613b91dbac45c
Donald Boulton <donaldboulton@gmail.com>
2019-08-17 12:38:36 -0500

    export twice

--- src/components/Ratings/Ratings.jsx
+++ src/components/Ratings/Ratings.jsx
@@ -2,51 +2,51 @@ import React from 'react'
 import styled from 'styled-components'
 import moment from 'moment'
 
-export default function Ratings(props) {
+export default function Ratings (props) {
   const {
     markdownRemark,
-    allRatingsJson: ratings = []
+    allRatingsJson: ratings = [],
   } = props.data
   const { frontmatter, first } = markdownRemark
 
-    const rating = this.props.data.allRatingsJson.edges
-    const ratingValue =
+  const rating = this.props.data.allRatingsJson.edges
+  const ratingValue =
       ratings && ratings.edges
         ? ratings.edges.reduce(
           (accumulator, rating) => accumulator + parseInt(rating.node.rating),
           0
         ) / ratings.totalCount
         : 0
-    const ratingCount = ratings && ratings.edges ? ratings.totalCount : 0
+  const ratingCount = ratings && ratings.edges ? ratings.totalCount : 0
 
-    return (
-      <Rating first={first}>
-        <div
-          data={{
-            ...frontmatter,
-            rating: { ratingValue, ratingCount: ratingCount },
-          }}
-          rich
-        />
-        {ratings ? (
-          <RatingDiv>
+  return (
+    <Rating first={first}>
+      <div
+        data={{
+          ...frontmatter,
+          rating: { ratingValue, ratingCount: ratingCount },
+        }}
+        rich
+      />
+      {ratings ? (
+        <RatingDiv>
           Rating: {ratingValue !== 0 ? ratingValue.toFixed(1) : null} -{' '}
-            {ratings.totalCount} Reviews
-          </RatingDiv>
-        ) : null}
-        {ratings && ratings.edges && ratings.edges.length ? (
-          ratings.edges.map((rating, i) => (
-            <Message key={rating.node.id} rating={rating} first={i === 0} />
-          ))
-        ) : (
-          <p>Be the first to post a rating on this post</p>
-        )}
-        <Message
-          dangerouslySetInnerHTML={{ __html: rating.fields.messageHtml }}
-        />
-        <Date>{moment.unix(rating.date).format('LLL')}</Date> by{' '}
-      </Rating>
-    )
+          {ratings.totalCount} Reviews
+        </RatingDiv>
+      ) : null}
+      {ratings && ratings.edges && ratings.edges.length ? (
+        ratings.edges.map((rating, i) => (
+          <Message key={rating.node.id} rating={rating} first={i === 0} />
+        ))
+      ) : (
+        <p>Be the first to post a rating on this post</p>
+      )}
+      <Message
+        dangerouslySetInnerHTML={{ __html: rating.fields.messageHtml }}
+      />
+      <Date>{moment.unix(rating.date).format('LLL')}</Date> by{' '}
+    </Rating>
+  )
 }
 
 const Rating = styled.div`
@@ -68,4 +68,3 @@ const RatingDiv = styled.div`
 const Date = styled.span`
   font-size: .75rem
 `
-export default Ratings
