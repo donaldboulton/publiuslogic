8ee1a10c6c60e26eb8a741dca83610799d28468a
Donald Boulton <donaldboulton@gmail.com>
2019-08-20 17:07:33 -0500

    picture srcSet to tags and categories

--- src/pages/categories/index.js
+++ src/pages/categories/index.js
@@ -27,21 +27,31 @@ const CategoriesPage = ({
 }) => (
   <Global>
     <Helmet title={`Categories | ${title}`} />
-    <section className='hero hero-blog-cover'>
-      <div className='hero-body'>
-        <div className='container'>
-          <div className='columns'>
-            <div className='column is-10 is-offset-1'>
-              <div className='section'>
-                <Styledh1>
-                   All Site Categories
-                </Styledh1>
-              </div>
-            </div>
-          </div>
-        </div>
+    <section className='hero'>
+      <div className='gatsby-image-wrapper' style='position: relative; overflow: hidden; height: 100%; width: 100%;'>
+        <div style='width: 100%; padding-bottom: 24.5758%;' />
+        <img src='data:image/jpeg;base64,/9j/2wBDABALDA4MChAODQ4SERATGCgaGBYWGDEjJR0oOjM9PDkzODdASFxOQERXRTc4UG1RV19iZ2hnPk1xeXBkeFxlZ2P/2wBDARESEhgVGC8aGi9jQjhCY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2P/wgARCAAFABQDASIAAhEBAxEB/8QAFgABAQEAAAAAAAAAAAAAAAAAAAEF/8QAFgEBAQEAAAAAAAAAAAAAAAAAAAEC/9oADAMBAAIQAxAAAAHHCwM//8QAFBABAAAAAAAAAAAAAAAAAAAAEP/aAAgBAQABBQJ//8QAFREBAQAAAAAAAAAAAAAAAAAAEEH/2gAIAQMBAT8Bh//EABQRAQAAAAAAAAAAAAAAAAAAABD/2gAIAQIBAT8BP//EABQQAQAAAAAAAAAAAAAAAAAAABD/2gAIAQEABj8Cf//EABkQAAMAAwAAAAAAAAAAAAAAAAABIRFBYf/aAAgBAQABPyFVjmjPD//aAAwDAQACAAMAAAAQB9//xAAVEQEBAAAAAAAAAAAAAAAAAAAAEf/aAAgBAwEBPxAj/8QAFREBAQAAAAAAAAAAAAAAAAAAEEH/2gAIAQIBAT8Qp//EABkQAQEBAQEBAAAAAAAAAAAAAAERAEExof/aAAgBAQABPxCQJ3IUPHchbP3f/9k=' alt='' style='position: absolute; top: 0px; left: 0px; width: 100%; height: 100%; object-fit: cover; object-position: center center; opacity: 1; transition-delay: 500ms;' />
+        <picture>
+          <source type='image/webp' srcSet='/images/contact350w.webp 350w, /images/contact700w.webp 700w, /images/contact1400w.webp 1400w, /images/contact1827w.webp 1827w' sizes='(max-width: 1400px) 100vw, 1400px' />
+          <source srcSet='/images/contact350w.jpg 350w, /images/contact700w.jpg 700w, /images/contact1400w.jpg 1400w, /images/contact1827w.jpg 1827w' sizes='(max-width: 1400px) 100vw, 1400px' />
+          <img sizes='(max-width: 1400px) 100vw, 1400px' srcSet='/images/contact350w.jpg 350w, /images/contact700w.jpg 700w, /images/contact1400w.jpg 1400w, /images/contact1827w.jpg 1827w' src='/images/contact.jpg' alt='' loading='lazy' style='position: absolute; top: 0px; left: 0px; width: 100%; height: 100%; object-fit: cover; object-position: center center; opacity: 0; transition: opacity 500ms ease 0s;' />
+        </picture>
+        <noscript>
+          <picture>
+            <source type='image/webp' srcSet='/images/contact350w.webp 350w, /images/contact700w.webp 700w, /images/contact1400w.webp 1400w, /images/contact1827w.webp 1827w' sizes='(max-width: 1400px) 100vw, 1400px' />
+            <source srcSet='/images/contact350w.jpg 350w, /images/contact700w.jpg 700w, /images/contact1400w.jpg 1400w, /images/contact1827w.jpg 1827w' sizes='(max-width: 1400px) 100vw, 1400px' />
+            <img loading='lazy' sizes='(max-width: 1400px) 100vw, 1400px' srcSet='/images/contact350w.jpg 350w, /images/contact700w.jpg 700w, /images/contact1400w.jpg 1400w, /images/contact1827w.jpg 1827w' src='/images/contact.jpg' alt='' style='position:absolute;top:0;left:0;opacity:1;width:100%;height:100%;object-fit:cover;object-position:center' />
+          </picture>
+        </noscript>
       </div>
     </section>
+    <div className='column is-10 is-offset-1'>
+      <div className='section'>
+        <Styledh1>
+          Categories All Pages
+        </Styledh1>
+      </div>
+    </div>
     <section className='section'>
       <div className='container content'>
         <div className='columns'>
--- static/js/puctureset.js
+++ static/js/puctureset.js
@@ -45,34 +45,24 @@ export default class HTML extends Component {
           <link rel='shortcut icon' href={favicon} />
         </head>
         <body itemScope='itemScope' itemType='https://schema.org/WebPage'>
-          <section class="hero">
-            <div class=" gatsby-image-wrapper" style="position: relative; overflow: hidden; height: 100%; width: 100%;">
-              <div style="width: 100%; padding-bottom: 24.5758%;"></div>
-                 <img src="data:image/jpeg;base64,/9j/2wBDABALDA4MChAODQ4SERATGCgaGBYWGDEjJR0oOjM9PDkzODdASFxOQERXRTc4UG1RV19iZ2hnPk1xeXBkeFxlZ2P/2wBDARESEhgVGC8aGi9jQjhCY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2P/wgARCAAFABQDASIAAhEBAxEB/8QAFgABAQEAAAAAAAAAAAAAAAAAAAEF/8QAFgEBAQEAAAAAAAAAAAAAAAAAAAEC/9oADAMBAAIQAxAAAAHHCwM//8QAFBABAAAAAAAAAAAAAAAAAAAAEP/aAAgBAQABBQJ//8QAFREBAQAAAAAAAAAAAAAAAAAAEEH/2gAIAQMBAT8Bh//EABQRAQAAAAAAAAAAAAAAAAAAABD/2gAIAQIBAT8BP//EABQQAQAAAAAAAAAAAAAAAAAAABD/2gAIAQEABj8Cf//EABkQAAMAAwAAAAAAAAAAAAAAAAABIRFBYf/aAAgBAQABPyFVjmjPD//aAAwDAQACAAMAAAAQB9//xAAVEQEBAAAAAAAAAAAAAAAAAAAAEf/aAAgBAwEBPxAj/8QAFREBAQAAAAAAAAAAAAAAAAAAEEH/2gAIAQIBAT8Qp//EABkQAQEBAQEBAAAAAAAAAAAAAAERAEExof/aAAgBAQABPxCQJ3IUPHchbP3f/9k=" alt="" style="position: absolute; top: 0px; left: 0px; width: 100%; height: 100%; object-fit: cover; object-position: center center; opacity: 1; transition-delay: 500ms;">
-                 <picture>
-                   <source type="image/webp" srcset="/static/095596255aae71099a75684562f32e8c/850cb/sitemap.webp 350w, /static/095596255aae71099a75684562f32e8c/93ec8/sitemap.webp 700w, /static/095596255aae71099a75684562f32e8c/f0780/sitemap.webp 1400w, /static/095596255aae71099a75684562f32e8c/79b8f/sitemap.webp 1827w" sizes="(max-width: 1400px) 100vw, 1400px"></source>
-                   <source srcset="/static/095596255aae71099a75684562f32e8c/804d2/sitemap.jpg 350w, /static/095596255aae71099a75684562f32e8c/ed516/sitemap.jpg 700w, /static/095596255aae71099a75684562f32e8c/abebb/sitemap.jpg 1400w, /static/095596255aae71099a75684562f32e8c/d603d/sitemap.jpg 1827w" sizes="(max-width: 1400px) 100vw, 1400px">
-                      <img sizes="(max-width: 1400px) 100vw, 1400px" srcset="/static/095596255aae71099a75684562f32e8c/804d2/sitemap.jpg 350w, /static/095596255aae71099a75684562f32e8c/ed516/sitemap.jpg 700w, /static/095596255aae71099a75684562f32e8c/abebb/sitemap.jpg 1400w, /static/095596255aae71099a75684562f32e8c/d603d/sitemap.jpg 1827w" src="/static/095596255aae71099a75684562f32e8c/abebb/sitemap.jpg" alt="" loading="lazy" style="position: absolute; top: 0px; left: 0px; width: 100%; height: 100%; object-fit: cover; object-position: center center; opacity: 0; transition: opacity 500ms ease 0s;" />
-                   </source>
-                 </picture>
-                    <noscript>
+          <section className='hero'>
+            <div className='gatsby-image-wrapper' style='position: relative; overflow: hidden; height: 100%; width: 100%;'>
+              <div style='width: 100%; padding-bottom: 24.5758%;' />
+              <img src='data:image/jpeg;base64,/9j/2wBDABALDA4MChAODQ4SERATGCgaGBYWGDEjJR0oOjM9PDkzODdASFxOQERXRTc4UG1RV19iZ2hnPk1xeXBkeFxlZ2P/2wBDARESEhgVGC8aGi9jQjhCY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2P/wgARCAAFABQDASIAAhEBAxEB/8QAFgABAQEAAAAAAAAAAAAAAAAAAAEF/8QAFgEBAQEAAAAAAAAAAAAAAAAAAAEC/9oADAMBAAIQAxAAAAHHCwM//8QAFBABAAAAAAAAAAAAAAAAAAAAEP/aAAgBAQABBQJ//8QAFREBAQAAAAAAAAAAAAAAAAAAEEH/2gAIAQMBAT8Bh//EABQRAQAAAAAAAAAAAAAAAAAAABD/2gAIAQIBAT8BP//EABQQAQAAAAAAAAAAAAAAAAAAABD/2gAIAQEABj8Cf//EABkQAAMAAwAAAAAAAAAAAAAAAAABIRFBYf/aAAgBAQABPyFVjmjPD//aAAwDAQACAAMAAAAQB9//xAAVEQEBAAAAAAAAAAAAAAAAAAAAEf/aAAgBAwEBPxAj/8QAFREBAQAAAAAAAAAAAAAAAAAAEEH/2gAIAQIBAT8Qp//EABkQAQEBAQEBAAAAAAAAAAAAAAERAEExof/aAAgBAQABPxCQJ3IUPHchbP3f/9k=' alt='' style='position: absolute; top: 0px; left: 0px; width: 100%; height: 100%; object-fit: cover; object-position: center center; opacity: 1; transition-delay: 500ms;' />
+              <picture>
+                <source type='image/webp' srcSet='/images/contact350w.webp 350w, /images/contact700w.webp 700w, /images/contact1400w.webp 1400w, /images/contact1827w.webp 1827w' sizes='(max-width: 1400px) 100vw, 1400px' />
+                <source srcSet='/images/contact350w.jpg 350w, /images/contact700.jpg 700w, /images/contact1400.jpg 1400w, /images/contact1827w.jpg 1827w' sizes='(max-width: 1400px) 100vw, 1400px' />
+                <img sizes='(max-width: 1400px) 100vw, 1400px' srcSet='/images/contact350w.jpg 350w, /images/contact700w.jpg 700w, /images/contact1400w.jpg 1400w, /images/contact1827w.jpg 1827w' src='/images/contact.jpg' alt='' loading='lazy' style='position: absolute; top: 0px; left: 0px; width: 100%; height: 100%; object-fit: cover; object-position: center center; opacity: 0; transition: opacity 500ms ease 0s;' />
+              </picture>
+              <noscript>
                 <picture>
-                  <source type='image/webp' srcset="/static/095596255aae71099a75684562f32e8c/850cb/sitemap.webp 350w,
-/static/095596255aae71099a75684562f32e8c/93ec8/sitemap.webp 700w,
-/static/095596255aae71099a75684562f32e8c/f0780/sitemap.webp 1400w,
-/static/095596255aae71099a75684562f32e8c/79b8f/sitemap.webp 1827w" sizes="(max-width: 1400px) 100vw, 1400px" /></source>
-<source srcset="/static/095596255aae71099a75684562f32e8c/804d2/sitemap.jpg 350w,
-/static/095596255aae71099a75684562f32e8c/ed516/sitemap.jpg 700w,
-/static/095596255aae71099a75684562f32e8c/abebb/sitemap.jpg 1400w,
-/static/095596255aae71099a75684562f32e8c/d603d/sitemap.jpg 1827w" sizes="(max-width: 1400px) 100vw, 1400px" /><img loading="lazy" sizes="(max-width: 1400px) 100vw, 1400px" srcset="/static/095596255aae71099a75684562f32e8c/804d2/sitemap.jpg 350w,
-/static/095596255aae71099a75684562f32e8c/ed516/sitemap.jpg 700w,
-/static/095596255aae71099a75684562f32e8c/abebb/sitemap.jpg 1400w,
-/static/095596255aae71099a75684562f32e8c/d603d/sitemap.jpg 1827w" src="/static/095596255aae71099a75684562f32e8c/abebb/sitemap.jpg" alt="" style="position:absolute;top:0;left:0;opacity:1;width:100%;height:100%;object-fit:cover;object-position:center" />
-</source>
-</picture>
-</noscript>
-              </div>
-            </section>
+                  <source type='image/webp' srcSet='/images/contact350w.webp 350w, /images/contact.webp 700w, /images/contact.webp 1400w, /images/contact1827w.webp 1827w' sizes='(max-width: 1400px) 100vw, 1400px' />
+                  <source srcSet='/images/contact350w.jpg 350w, /images/contact.jpg 700w, /images/contact.jpg 1400w, /images/contact1827w.jpg 1827w' sizes='(max-width: 1400px) 100vw, 1400px' />
+                  <img loading='lazy' sizes='(max-width: 1400px) 100vw, 1400px' srcSet='/images/contact.jpg 350w, /images/contact700w.jpg 700w, /images/contact1400w.jpg 1400w, /images/contact1827w.jpg 1827w' src='/images/contact.jpg' alt='' style='position:absolute;top:0;left:0;opacity:1;width:100%;height:100%;object-fit:cover;object-position:center' />
+                </picture>
+              </noscript>
+            </div>
+          </section>
           <div
             id='___gatsby'
             dangerouslySetInnerHTML={{ __html: this.props.body }}
