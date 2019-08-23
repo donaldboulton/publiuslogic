5bf8305345c9246bc336dbc5f6be29d92065c2af
Donald Boulton <donaldboulton@gmail.com>
2019-08-23 08:24:37 -0500

    fix multiple img folders

--- gatsby-config.js
+++ gatsby-config.js
@@ -82,13 +82,6 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        path: `${__dirname}/src/assets/img`,
-        name: 'icons',
-      },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
         path: `${__dirname}/src/pages`,
         name: 'pages',
       },
