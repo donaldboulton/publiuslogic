91dc3fabba763c508b767593620bf211fed42ff9
Donald Boulton <donaldboulton@gmail.com>
2019-10-13 11:15:14 -0500

    404 work

--- src/components/Knockout/index.js
+++ src/components/Knockout/index.js
@@ -11,7 +11,7 @@ const Knockout = () => {
         <>
           <div className='knockout'>
             <svg className='knockout-text-container' width='100%' height='100%'>
-              <rect className='knockout-text-bg' width='100%' height='100%' fill='#1d1d1d' x='0' y='0' fillOpacity='1' mask='url(#knockout-text)' />
+              <rect className='knockout-text-bg' width='110%' height='100%' fill='transparent' x='0' y='0' fillOpacity='1' mask='url(#knockout-text)' />
               <mask id='knockout-text'>
                 <rect width='100%' height='100%' fill='#fff' x='0' y='0' />
                 <text x='50%' y='175' fill='#d64000' textAnchor='middle'>404: NOT FOUND</text>
--- src/pages/404.js
+++ src/pages/404.js
@@ -8,12 +8,12 @@ const NotFoundPage = () => (
     <Helmet>
       <title>Error 404 | PubliusLogic</title>
     </Helmet>
-    <section className='hero is-primary is-bold is-medium'>
+    <section className='hero'>
       <div className='hero-body'>
         <div className='container'>
           <div className='columns'>
-            <div className='column is-10 is-offset-1'>
-              <div className='section is-centered'>
+            <div className='column'>
+              <div className='is-centered'>
                 <div className='column'>
                   <KnockOut />
                 </div>
@@ -25,6 +25,17 @@ const NotFoundPage = () => (
               </div>
             </div>
           </div>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div className='column is-10 is-offset-1'>
+        <div className='is-centered'>
+          <h2 className='subtitle'>
+                  You just hit a route that doesn&#39;t exist... the
+                  sadness.
+          </h2>
+                Click send the email icon below to send us a email about the error!
         </div>
       </div>
     </section>
