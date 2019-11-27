4671ed884f33ae804830db001bb8001ff791cce1
Donald Boulton <donaldboulton@gmail.com>
2019-11-26 23:19:06 -0600

    smaller search input smaller toggle stayled icons

--- src/components/DarkMode/DarkModeToggle.js
+++ src/components/DarkMode/DarkModeToggle.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Moon } from 'styled-icons/fa-solid/Moon'
+import { Sun } from 'styled-icons/fa-solid/Sun'
 import Toggle from './Toggle'
 import useDarkMode from 'use-dark-mode'
 import './styles.scss'
@@ -10,11 +12,11 @@ const DarkModeToggle = () => {
     <span className='navbar-item'>
       <div className='dark-mode-toggle'>
         <button type='button' onClick={darkMode.disable}>
-          ☀
+          <Sun size='1em' />
         </button>
         <Toggle checked={darkMode.value} onChange={darkMode.toggle} />
         <button type='button' onClick={darkMode.enable}>
-          ☾
+          <Moon size='1em' />
         </button>
       </div>
     </span>
