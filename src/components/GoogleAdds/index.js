62e9ac5009cca19081da5df6226bbcd7c42dc9e2
Donald Boulton <donaldboulton@gmail.com>
2020-03-12 13:56:18 -0500

    Google ads killing pages

--- src/components/GoogleAdds/index.js
+++ src/components/GoogleAdds/index.js
@@ -1,18 +1,17 @@
 import React from 'react'
 import AdSense from 'react-adsense'
-import { AddsDiv } from './styles'
 
 const Adds = () => {
   return (
-    <AddsDiv role='note' itemScope='itemScope' itemType='https://schema.org/WPAdBlock'>
+    <div role='note' className='add-block clear-both' itemScope='itemScope' itemType='https://schema.org/WPAdBlock'>
       <AdSense.Google
         client='ca-pub-7655495105068461'
         slot='5601054893'
-        style={{ display: 'block', maxWidth: 23 }}
-        layout='in-article'
+        style={{ display: 'block' }}
+        layout='display'
         format='fluid'
       />
-    </AddsDiv>
+    </div>
   )
 }
 
